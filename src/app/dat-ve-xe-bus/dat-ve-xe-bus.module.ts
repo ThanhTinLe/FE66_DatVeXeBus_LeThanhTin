235d963738279890a0378e9267f81bec6c623883
RayKhum113 <tinltse140972@gmail.com>
2021-08-20 17:32:20 +0700

    update

--- src/app/dat-ve-xe-bus/dat-ve-xe-bus.module.ts
+++ src/app/dat-ve-xe-bus/dat-ve-xe-bus.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChonGheComponent } from './chon-ghe/chon-ghe.component';
-import { GetDaChonComponent } from './get-da-chon/get-da-chon.component';
 import { DatVeXeBusComponent } from './dat-ve-xe-bus/dat-ve-xe-bus.component';
 
 
 
 @NgModule({
   declarations: [
-    ChonGheComponent,
-    GetDaChonComponent,
     DatVeXeBusComponent
   ],
   imports: [
